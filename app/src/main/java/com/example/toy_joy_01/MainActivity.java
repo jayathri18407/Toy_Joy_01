de09a3903b5fbaa813c461d358f85d255c6d600d
jayathri18407 <60905646+jayathri18407@users.noreply.github.com>
2020-04-29 20:01:01 +0530

    this is the last

---
+++ app/src/main/java/com/example/toy_joy_01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.toy_joy_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
